5bcf2ee5566cace46379df782f61a67c9c6c6d5d
Sreekar Rallabandi <bannu.sreekar@gmail.com>
2022-11-03 15:34:25 +0530

    modification2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="wafer-fault-detection",
     version="0.0.3",
     description="ML project",
-    author="Avnish yadav", 
+    author="Sreekar Rallabandi",
     packages=find_packages(),
     license="MIT"
 )
